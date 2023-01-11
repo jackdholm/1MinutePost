17c1d8913edda1c865b0d550b490a435be3b04d0
Jack Holm <jackdholm1@gmail.com>
2023-01-11 17:57:00 -0600

    Added updater

---
+++ 1MinutePost_Update/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Reflection;
+
+var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+if (environmentName == null)
+    environmentName = "Production";
+var config = new ConfigurationBuilder()
+                 .SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location))
+                 .AddJsonFile($"appsettings.{environmentName}.json", optional: false, reloadOnChange: true)
+                 .AddUserSecrets<Program>()
+                 .Build();
+
+string connectionString = config.GetConnectionString("mpostDB");
+
+await using var connection   = new NpgsqlConnection(connectionString);
+await connection.OpenAsync();
+
+DateTime time = DateTime.UtcNow.AddHours(-1);
+
+await using (var command = new NpgsqlCommand("DELETE FROM posts WHERE created < @p", connection))
+{
+    command.Parameters.AddWithValue("p", time);
+    await command.ExecuteNonQueryAsync();
+}
