49cfe2da6887d24cfb07d84fb6b9e449c7470b64
Jack Holm <jackdholm1@gmail.com>
2023-01-31 19:22:44 -0600

    Timer now auto-updates when voting

--- 1MinutePost/ClientApp/src/app/post/post.component.ts
+++ 1MinutePost/ClientApp/src/app/post/post.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
-import { CountdownConfig, CountdownEvent, CountdownGlobalConfig } from 'ngx-countdown';
+import { CountdownComponent, CountdownConfig, CountdownEvent, CountdownGlobalConfig } from 'ngx-countdown';
 import { Observable } from 'rxjs';
 import { VoteService } from '../Services/vote-service.service';
 
@@ -32,8 +32,12 @@ export class PostComponent implements OnInit
   {
     this.arrowVoted = this.Voted === "true" ? true : false;
     this.numberVotes = +this.VoteCount;
+    this.configureTime();
+  }
+
+  configureTime() {
     var time = 600000 - (new Date().getTime() - new Date(this.Created).getTime()) + 60000 * this.numberVotes;
-    this.TimeConfig.leftTime = time / 1000;
+    this.TimeConfig = { leftTime: time / 1000, format: 'm:ss' };
   }
 
   HandleEvent(e: CountdownEvent)
@@ -51,6 +55,7 @@ export class PostComponent implements OnInit
     else {
       this.arrowVoted = true;
       this.numberVotes++;
+      this.configureTime();
     }
   }
 }
